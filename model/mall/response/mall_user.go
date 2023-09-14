2146fee015604691c62b48a08677172e6fd9f2b2
黄俊杰 <447992733@qq.com>
2023-09-14 15:24:52 +0800

    用户信息

--- model/mall/response/mall_user.go
+++ model/mall/response/mall_user.go
@@ -4,4 +4,7 @@ type MallUserDetailResponse struct {
 	NickName      string `json:"nickName"`
 	LoginName     string `json:"loginName"`
 	IntroduceSign string `json:"introduceSign"`
+	UserId        int    `json:"userId"`
+	BscAddress    string `json:"bscAddress"`
+	LoginType     int    `json:"loginType"`
 }
